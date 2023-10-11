cbb6837ebdcf4ad5e7167b16a771b3a12e2eafab
Garik <32434637+Gathrik@users.noreply.github.com>
2023-10-11 15:53:25 +0200

    inputfields rood en paragraph positioning

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dayInput = document.querySelector("#day"); // Get day input field
 const monthInput = document.querySelector("#month"); // Get month input field
 const yearInput = document.querySelector("#year"); // Get year input field
+const inputFields =  document.querySelectorAll('.input-field')
 const submitButton = document.querySelector("#submitButton");
 const yearsResult = document.querySelector("#yearsResult");
 const monthsResult = document.querySelector("#monthsResult");
@@ -12,18 +13,61 @@ const calculateDate = () => {
     const month = parseInt(monthInput.value);
     const year = parseInt(yearInput.value);
 
-    // Validation checks
-    if (isNaN(day) || isNaN(month) || isNaN(year)) {
-        clearResults();
-        displayError("Please fill in all fields");
-        return;
+    displayError("");
+
+    for (const field of inputFields) {
+        field.classList.remove('input-error');
+        const id = field.getAttribute('id');
+        const value = field.value.trim();
+
+        if (id === 'day') {
+            if (value === '' || isNaN(value)) {
+                displayError(field, 'Please fill in day field');
+            }
+
+            if (value < 1 || value > 31) {
+                displayError(field, 'Please enter a valid date');
+            }
+        }
+
+        else if (id === 'month') {
+            if (value === '' || isNaN(value)) {
+                displayError(field, 'Please fill in month field');
+            }
+
+            if (value < 1 || value > 12) {
+                displayError(field, 'Please enter a valid date');
+            }
+        }
+
+        else if (id === 'year') {
+            if (value === '' || isNaN(value)) {
+                displayError(field, 'Please fill in year field');
+            }
+
+            if (value < 1) {
+                displayError(field, 'Please enter a valid date');
+            }
+
+            if (value > new Date().getFullYear()) {
+                displayError(field, 'Please enter a date in the past');
+            }
+        }
     }
 
-    if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1) {
-        clearResults();
-        displayError("Please enter a valid date");
-        return;
-    }
+    // // Validation checks
+    // if (isNaN(day) || isNaN(month) || isNaN(year)) {
+    //     // clearResults();
+    //     displayError("Please fill in all fields");
+    //
+    //     return;
+    // }
+    //
+    // if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1) {
+    //     // clearResults();
+    //     displayError("Please enter a valid date");
+    //     return;
+    // }
 
     const inputDate = new Date(year, month - 1, day);
     const currentDate = new Date();
@@ -37,26 +81,32 @@ const calculateDate = () => {
     const months = Math.floor((timeDifference % millisecondsPerYear) / millisecondsPerMonth);
     const days = Math.floor(((timeDifference % millisecondsPerYear) % millisecondsPerMonth) / millisecondsPerDay);
 
-    if (inputDate > currentDate) {
-        clearResults();
-        displayError("Please enter a date in the past");
-        return;
-    }
+    // if (inputDate > currentDate) {
+    //     // clearResults();
+    //     displayError("Please enter a date in the past");
+    //     return;
+    // }
 
-    yearsResult.textContent = `${years} years`;
-    monthsResult.textContent = `${months} months`;
-    daysResult.textContent = `${days} days`;
+    yearsResult.textContent = `${years}`;
+    monthsResult.textContent = `${months}`;
+    daysResult.textContent = `${days}`;
 };
 
-const clearResults = () => {
-    yearsResult.textContent = "";
-    monthsResult.textContent = "";
-    daysResult.textContent = "";
-};
+// const clearResults = () => {
+//     yearsResult.textContent = "";
+//     monthsResult.textContent = "";
+//     daysResult.textContent = "";
+// };
 
-const displayError = (message) => {
+// const displayError = (inputElement, message) => {
+//     errorResult.textContent = message;
+//     inputElement.classList.add("input-error");
+// };
+
+function displayError(inputElement, message) {
     errorResult.textContent = message;
-};
+    inputElement.classList.add("input-error");
+}
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
